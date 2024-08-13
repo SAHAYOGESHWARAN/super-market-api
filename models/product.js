be5215d68dfff9270b98516d7ff3253c88a6620d
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-13 09:03:36 +0530

    add super markt master

--- models/product.js
+++ models/product.js
@@ -4,6 +4,8 @@ const ProductSchema = new mongoose.Schema({
     name: { type: String, required: true },
     price: { type: Number, required: true },
     productId: { type: String, required: true, unique: true },
+    updatedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // Reference to User model
+    updatedAt: { type: Date, default: Date.now }, // Timestamp of the update
 });
 
 module.exports = mongoose.model('Product', ProductSchema);
