8d6794a67576ae41e059e764495aceb7b1f61b5c
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-13 09:07:01 +0530

    add super market master

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,49 @@
 const Product = require('../models/product');
 
+// Add a new product
+const addProduct = async (req, res) => {
+    const { name, price, productId } = req.body;
+
+    const productExists = await Product.findOne({ productId });
+    if (productExists) {
+        return res.status(400).json({ message: 'Product ID already exists' });
+    }
+
+    const product = await Product.create({
+        name,
+        price,
+        productId,
+    });
+
+    if (product) {
+        res.status(201).json({
+            _id: product._id,
+            name: product.name,
+            price: product.price,
+            productId: product.productId,
+        });
+    } else {
+        res.status(400).json({ message: 'Invalid product data' });
+    }
+};
+
+// Get all products
+const getProducts = async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+};
+
+// Get a single product by ID
+const getProductById = async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+        res.json(product);
+    } else {
+        res.status(404).json({ message: 'Product not found' });
+    }
+};
+
 // Update a product
 const updateProduct = async (req, res) => {
     const { id } = req.params;
@@ -28,4 +72,5 @@ const updateProduct = async (req, res) => {
     });
 };
 
+// Export all functions
 module.exports = { addProduct, getProducts, getProductById, updateProduct };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,20 +1,18 @@
 const express = require('express');
-const { addProduct, getProducts, getProductById } = require('../controllers/productController');
+const { addProduct, getProducts, getProductById, updateProduct } = require('../controllers/productController');
 const protect = require('../middleware/authMiddleware');
 const router = express.Router();
-const { purchaseProduct } = require('../controllers/purchaseController');
 
 // Admin adds a new product (protected route)
 router.post('/add', protect, addProduct);
+
+// Admin updates a product (protected route)
+router.put('/update/:id', protect, updateProduct);
 
 // User views all products
 router.get('/', getProducts);
 
 // User views a single product by ID
 router.get('/:id', getProductById);
-
-// User purchases a product (protected route)
-router.post('/', protect, purchaseProduct);
-
 
 module.exports = router;
