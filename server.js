6ebd4135733675f2c73123209a5d9ee4e2a3b3af
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-13 08:24:43 +0530

    add super market master

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const adminRoutes = require('./routes/adminRoutes');
 require('dotenv').config();
 
 
-dotenv.config();
+
 connectDB();
 
 const app = express();
@@ -15,10 +15,9 @@ app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api/admins', adminRoutes);
 
-//port
-const PORT = process.env.PORT || 5000;
 
-//listen
-app.listen(PORT, () => {
-  console.log(`Node Server Running In${process.env.DEV_MODE} Mode on port no ${PORT}`);
-});
+
+const PORT = process.env.PORT || 5000;
+const DEV_MODE = process.env.DEV_MODE || 'development';
+
+console.log(`Node Server Running In ${DEV_MODE} Mode on port no ${PORT}`);
