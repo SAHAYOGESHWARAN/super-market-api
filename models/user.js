5ae919317cfcf2c94ba7a0aaa2d93412a377067b
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-13 17:59:03 +0530

    add super market master

--- models/user.js
+++ models/user.js
@@ -6,7 +6,11 @@ const userSchema = mongoose.Schema(
             type: String,
              required: true 
             },
-        email: { type: String, required: true, unique: true },
+        email: { 
+            type: String,
+             required: true,
+              unique: true 
+            },
         password: { type: String, required: true },
         productList: [
             {
