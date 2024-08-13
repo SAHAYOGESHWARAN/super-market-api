18af09aa3803e379c18f270cd14b9b75b13ef69b
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-13 11:06:17 +0530

    add super market master

--- middleware/authmiddleware.js
+++ middleware/authmiddleware.js
@@ -4,12 +4,18 @@ const Admin = require('../models/admin');
 
 const protect = async (req, res, next) => {
     let token;
+
+    // Check for token in headers
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         try {
+            // Get token from header
             token = req.headers.authorization.split(' ')[1];
+
+            // Verify token
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
-            req.user = await User.findById(decoded.id).select('-password');
-            req.admin = await Admin.findById(decoded.id).select('-password');
+
+            // Attach user to request
+            req.user = await User.findById(decoded.id).select('-password'); // Exclude password field
             next();
         } catch (error) {
             res.status(401).json({ message: 'Not authorized, token failed' });
