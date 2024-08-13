8386fc46e25993beabca65a4164b191d732d0409
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-13 11:15:30 +0530

    add super market master

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,16 +4,9 @@ const protect = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
-// Route to add a new product
 router.post('/add', protect, addProduct);
-
-// Route to get all products
 router.get('/', getProducts);
-
-// Route to get a product by productId
 router.get('/:productId', getProductById);
-
-// Route to update a product by productId
 router.put('/update/:productId', protect, updateProduct);
 
 module.exports = router;
