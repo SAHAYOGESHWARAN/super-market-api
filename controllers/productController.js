f047d6d3e910368bc3e40815d7eacb34972ab421
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-13 09:19:31 +0530

    add super market master

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,48 +1,6 @@
 const Product = require('../models/product');
-
-// Add a new product
-const addProduct = async (req, res) => {
-    const { name, price, productId } = req.body;
-
-    const productExists = await Product.findOne({ productId });
-    if (productExists) {
-        return res.status(400).json({ message: 'Product ID already exists' });
-    }
-
-    const product = await Product.create({
-        name,
-        price,
-        productId,
-    });
-
-    if (product) {
-        res.status(201).json({
-            _id: product._id,
-            name: product.name,
-            price: product.price,
-            productId: product.productId,
-        });
-    } else {
-        res.status(400).json({ message: 'Invalid product data' });
-    }
-};
-
-// Get all products
-const getProducts = async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-};
-
-// Get a single product by ID
-const getProductById = async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-        res.json(product);
-    } else {
-        res.status(404).json({ message: 'Product not found' });
-    }
-};
+const User = require('../models/user'); // Import User model
+const Admin = require('../models/admin'); // Import Admin model (if applicable)
 
 // Update a product
 const updateProduct = async (req, res) => {
@@ -57,8 +15,11 @@ const updateProduct = async (req, res) => {
 
     product.name = name || product.name;
     product.price = price || product.price;
-    product.updatedBy = req.user ? req.user._id : req.admin._id; // Capture the user/admin who made the update
-    product.updatedAt = Date.now(); // Update the timestamp
+
+    // Identify who is updating the product
+    const updater = req.user ? await User.findById(req.user._id) : await Admin.findById(req.admin._id);
+    product.updatedBy = updater._id;
+    product.updatedAt = Date.now();
 
     const updatedProduct = await product.save();
 
@@ -67,10 +28,12 @@ const updateProduct = async (req, res) => {
         name: updatedProduct.name,
         price: updatedProduct.price,
         productId: updatedProduct.productId,
-        updatedBy: updatedProduct.updatedBy,
+        updatedBy: {
+            id: updater._id,
+            name: updater.name, // Assuming the User/Admin model has a 'name' field
+        },
         updatedAt: updatedProduct.updatedAt,
     });
 };
 
-// Export all functions
 module.exports = { addProduct, getProducts, getProductById, updateProduct };
