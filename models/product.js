6beaa10d14a96fa90ac62a5a3a83c4559bcd08b3
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-13 17:37:15 +0530

    add super market master

--- models/product.js
+++ models/product.js
@@ -1,23 +1,43 @@
 const mongoose = require('mongoose');
 
-const productSchema = mongoose.Schema(
-    {
-        name: { type: String, required: true },
-        price: { type: Number, required: true },
-        productId: { type: String, required: true, unique: true },
-        updatedBy: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User', // or 'Admin', depending on who updates
-        },
-        updatedAt: {
-            type: Date,
-            default: Date.now,
-        },
+const productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
     },
-    {
-        timestamps: true,
-    }
-);
+    price: {
+        type: Number,
+        required: true,
+    },
+    productId: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    updatedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    updatedAt: {
+        type: Date,
+    },
+    updateHistory: [
+        {
+            updatedBy: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User',
+            },
+            name: String,
+            price: Number,
+            updatedAt: {
+                type: Date,
+                default: Date.now,
+            },
+        },
+    ],
+}, {
+    timestamps: true,
+});
 
 const Product = mongoose.model('Product', productSchema);
 
