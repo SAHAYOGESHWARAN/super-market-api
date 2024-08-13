576c3e5d113975aa8d66ce2d08992dd7760421e3
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-13 08:58:34 +0530

    add super market master

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { addProduct, getProducts, getProductById } = require('../controllers/productController');
 const protect = require('../middleware/authMiddleware');
 const router = express.Router();
+const { purchaseProduct } = require('../controllers/purchaseController');
 
 // Admin adds a new product (protected route)
 router.post('/add', protect, addProduct);
@@ -11,5 +12,9 @@ router.get('/', getProducts);
 
 // User views a single product by ID
 router.get('/:id', getProductById);
+
+// User purchases a product (protected route)
+router.post('/', protect, purchaseProduct);
+
 
 module.exports = router;
