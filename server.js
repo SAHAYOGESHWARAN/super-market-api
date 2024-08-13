4746cfed54d3eff7fe1fe2cc514c46fc2ccca80d
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-13 08:33:52 +0530

    add super market master

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 const productRoutes = require('./routes/productRoutes');
+const purchaseRoutes = require('./routes/purchaseRoutes');
 
 dotenv.config();
 connectDB();
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api/admins', adminRoutes);
 app.use('/api/products', productRoutes);
+app.use('/api/purchase', purchaseRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
